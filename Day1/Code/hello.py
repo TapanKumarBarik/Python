8bccfd081100a2267ec8ac66a29ca1c982abd7af
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-06-04 12:45:18 +0530

    Day 1

--- Day1/Code/hello.py
+++ Day1/Code/hello.py
@@ -2,3 +2,24 @@ print("hello")
 sum = 10
 sum += 10
 print(sum)
+
+# Comment
+
+print("Hello World\nHello World")
+print("Hello " + "Tapan")
+
+
+# INPUT
+# name = input("What is your name")
+# lenthOfName = len(name)
+
+# print("Your name " + name + " is " + str(lenthOfName) + " characters long")
+
+a = input("A: ")
+b = input("B: ")
+
+c = a
+a = b
+b = c
+print("A:= " + a)
+print("B:= " + b)
---
+++ Day1/Code/proj.py
@@ -0,0 +1,7 @@
+print("Welcome to the brand name genarator page:")
+print("What is the name of the city you grow up in?")
+city_name = input()
+print("what is your pet name?")
+pet_name = input()
+res = city_name + " " + pet_name
+print("Your brand name could be " + res)
