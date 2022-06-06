40f864a10246afc4788d53916b40e31d52cd9c2a
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-06-06 09:08:20 +0530

    Day2

---
+++ Day2/main.py
@@ -0,0 +1,20 @@
+# Data Types
+
+# String
+
+print("Hello"[1])
+
+print("123" + "456")
+
+# integer
+print(123 + 456)
+
+print(type(11))
+
+# challange
+
+print("Write a number with 2 digits\n")
+var_num = input()
+
+
+print(int(var_num[0]) + int(var_num[1]))
